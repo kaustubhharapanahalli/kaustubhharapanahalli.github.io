88426606ab55288a9789302239805ead3d838d14
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2024-10-02 18:50:01 -0700

    Add Arxiv Button Component for indicating publications

---
+++ src/components/ArxivButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../css/Homepage.css"; // Ensure this file contains the updated styles
+
+const ArxivButton = ({ link }) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <Button className="arxiv-btn">
+        <img
+          src={`${process.env.PUBLIC_URL}/Assets/images/arxiv.png`}
+          alt="ArXiv"
+          style={{
+            width: "36px", // Adjusted width for the Arxiv logo
+            height: "18px",
+            objectFit: "contain",
+          }}
+        />
+      </Button>
+    </a>
+  );
+};
+
+export default ArxivButton;
