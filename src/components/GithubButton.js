9bcd9669072b8fc5c6766db6fb751e32de18a084
Kaustubh Harapanahalli <kaustubhharapanahalli@gmail.com>
2024-10-02 18:50:18 -0700

    Add Github Button Component for indicating in publications

---
+++ src/components/GithubButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../css/Homepage.css"; // Ensure this file contains the updated styles
+
+const GithubButton = ({ link }) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <Button className="github-btn">
+        <img
+          src={`${process.env.PUBLIC_URL}/Assets/images/github.png`}
+          alt="GitHub"
+          style={{ width: "18px", height: "18px" }} // Adjusted size for the GitHub logo
+        />
+        <span>Code</span>
+      </Button>
+    </a>
+  );
+};
+
+export default GithubButton;
